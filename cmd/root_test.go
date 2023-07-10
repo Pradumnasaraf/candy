c27bfb3bb382817d91b27ebd75e68231a0b826be
Pradumna saraf <pradumnasaraf@gmail.com>
2023-07-11 00:20:24 +0530

    feat: Add tests

---
+++ cmd/root_test.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+)
+
+func TestRootCmd(t *testing.T) {
+
+	expectedOutput := "Do all your tedious tasks with a single command"
+
+	cmd := exec.Command("candy")
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Errorf("expected no error, but got: %v", err)
+	}
+
+	got := string(output)[:47]
+	if got != expectedOutput {
+		fmt.Println(got)
+		fmt.Println(expectedOutput)
+		t.Errorf("expected %v, but got: %v", expectedOutput, got)
+	}
+
+}
